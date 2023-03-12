19b8de16ce4529b647e5653cce7b7125f685c5f8
Yusril <yusrilchalif@gmail.com>
2023-03-12 19:43:18 +0700

    init

---
+++ Weather_app/Weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_appApp.swift
+//  Weather_app
+//
+//  Created by Yusril on 12/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
